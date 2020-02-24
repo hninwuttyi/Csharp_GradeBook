9e73b803c713b8cc431071fb0313f7e5698f9290
Hnin Wuttyi <wuttyi@solidplm.com>
2020-02-24 10:03:06 +0630

    ,Array ,list , looping and format

--- Csharp_GradeBook/Program.cs
+++ Csharp_GradeBook/Program.cs
@@ -10,6 +10,26 @@ namespace Csharp_GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new double[] { 12.7,10.3,6.11,4.1};
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(double number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count();
+            Console.WriteLine($"The average grade is {result:N1}");
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine($"Hello!");
+            }
         }
     }
 }
