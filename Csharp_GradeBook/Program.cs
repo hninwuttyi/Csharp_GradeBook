16a844d2b04c52839747802509cbc9dcb7a5795b
Hnin Wuttyi <wuttyi@solidplm.com>
2020-04-11 22:19:24 +0630

    Re learning from the beginning.

--- Csharp_GradeBook/Program.cs
+++ Csharp_GradeBook/Program.cs
@@ -10,25 +10,16 @@ namespace Csharp_GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new double[] { 12.7,10.3,6.11,4.1};
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
+            var numbers = new double[3];
+            numbers[0] = 12.7;
 
-            var result = 0.0;
-            foreach(double number in grades)
-            {
-                result += number;
-            }
-            result /= grades.Count();
-            Console.WriteLine($"The average grade is {result:N1}");
-
-            if (args.Length > 0)
+            if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
             }
             else
             {
-                Console.WriteLine($"Hello!");
+                Console.WriteLine("Hello!");
             }
         }
     }
